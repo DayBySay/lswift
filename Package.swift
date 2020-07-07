1a963e87d084324197820974ea94b529a8b30d3e
daybysay <yukita.sekai+github@gmail.com>
2020-07-07 10:21:59 +0900

    implemented lswift base

--- Package.swift
+++ Package.swift
@@ -5,16 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "lswift",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .executable(name: "lswift", targets: ["lswift"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("master")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "lswift",
-            dependencies: []),
+            dependencies: [
+                .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
+            ]),
         .testTarget(
             name: "lswiftTests",
             dependencies: ["lswift"]),
--- Sources/lswift/main.swift
+++ Sources/lswift/main.swift
@@ -1,1 +1,49 @@
-print("Hello, world!")
+import Foundation
+import TSCUtility
+
+class Executor {
+    private let shouldShowDodFiles: OptionArgument<Bool>
+    
+    init(parser: ArgumentParser) {
+        shouldShowDodFiles = parser.add(option: "--show-dotfiles",
+                                        shortName: "-a",
+                                        kind: Bool.self,
+                                        usage: "Include directory entries whose names begin with a dot (.).")
+    }
+    
+    func execute(args: ArgumentParser.Result) {
+//        print(args)
+        let fileManager = FileManager.default
+        let currentPath = fileManager.currentDirectoryPath
+//        print(fileManager.currentDirectoryPath)
+
+        let shouldShowDotFiles = args.get(self.shouldShowDodFiles) ?? false
+        
+        do {
+            let fileURLs = try fileManager.contentsOfDirectory(atPath: currentPath)
+//            print(fileURLs)
+            let output = fileURLs.filter { shouldShowDotFiles ? true : $0.prefix(1) != "." }.reduce("") { (res, add) in
+                res + "\t\(add)"
+            }
+            print(output.dropFirst())
+        } catch {
+            print("Error while enumerating files \(currentPath): \(error.localizedDescription)")
+        }
+    }
+}
+
+func main() {
+    let parser = ArgumentParser(usage: "<options>", overview: "lswift: ls command made with swift language.")
+    
+    let command = Executor(parser: parser)
+    let inputs = Array(CommandLine.arguments.dropFirst())
+    
+    do {
+        let args = try parser.parse(inputs)
+        command.execute(args: args)
+    } catch {
+        fatalError("Command-line pasing error: \(error)")
+    }
+}
+
+main()
